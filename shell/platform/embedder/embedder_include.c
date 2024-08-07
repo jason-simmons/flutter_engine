0801c4d76a90627ad19e29500b21eb212c1c18b8
Jason Simmons <jsimmons@google.com>
2024-08-07 01:56:00 +0000

    remove flutter/ from impeller/runtime_stage and embedder_include paths

--- shell/platform/embedder/embedder_include.c
+++ shell/platform/embedder/embedder_include.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "flutter/shell/platform/embedder/embedder.h"
+#include "shell/platform/embedder/embedder.h"
 
 // This file is only present to ensure that the public embedder header can be
 // cleanly included in a C file.
--- shell/platform/embedder/embedder_include2.c
+++ shell/platform/embedder/embedder_include2.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "flutter/shell/platform/embedder/embedder.h"
+#include "shell/platform/embedder/embedder.h"
 
 // This file is the same as embedder_include.c and ensures that static methods
 // don't end up in public API header. This will cause duplicate symbols when the
